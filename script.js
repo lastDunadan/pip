06ee911872138fe4aad418c5b1fe6141f2557b56
pawel_modzelewski <pawel.modzelewski@creativemonsters.pl>
2021-05-11 09:02:37 +0200

    final version

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const domElement = {
     video: document.getElementById('video'),
     source: document.getElementById('source'),
-    button: document.getElementById('button')
+    button: document.getElementById('button'),
+    stream: null
 }
 
 //Select media stream ad pass to video element to play
 async function selectMedia() {
     try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-        console.log(mediaStream);
-        domElement.video.srcObject = mediaStream;
-        domElement.video.onloadedmetadata = () => {
-            domElement.video.play();
-        }
+        domElement.stream = await navigator.mediaDevices.getDisplayMedia();  
     } catch (error) {
-        console.log('error in selectMedia()');
+        console.error('error in selectMedia()', error);
     }
+}
+
+async function onVideoLoaded() {
+    domElement.video.play();
+    await domElement.video.requestPictureInPicture();
+    domElement.button.disabled = false;
 }
 
 domElement.button.addEventListener('click', async () => {
     // Disable buttton
     domElement.button.disabled = true;
-    //Start PIP
-    await domElement.video.requestPictureInPicture();
-    domElement.button.disabled = false;
+    domElement.video.onloadedmetadata = onVideoLoaded;
+    domElement.video.srcObject = domElement.stream;
+    
 });
 
 domElement.source.addEventListener('click', selectMedia);
